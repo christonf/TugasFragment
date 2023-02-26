4e36f94f693c5db2cc4fd90fe18513355d76e3d2
christonf <christophorus.ardhito0301@mail.ugm.ac.id>
2023-02-26 20:16:58 +0700

    Initial commit

--- app/src/main/java/com/example/tugasfragment/MainActivity.java
+++ app/src/main/java/com/example/tugasfragment/MainActivity.java
@@ -1,14 +1,62 @@
 package com.example.tugasfragment;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mButton;
+    TextView text;
+    private Boolean isFragmentDisplayed = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mButton = findViewById(R.id.k_button);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!isFragmentDisplayed) {
+                    displayFragment();
+                }
+                else {
+                    closeFragment();
+                }
+            }
+        });
+    }
+
+    public void displayFragment() {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        SimpleFragment simpleFragment = (SimpleFragment) fragmentManager.findFragmentById(R.id.fragment);
+
+        fragmentTransaction.replace(R.id.fragment, simpleFragment).commit();
+
+        isFragmentDisplayed =true;
+        text = findViewById(R.id.content);
+        text.setText(R.string.kelebihan);
+        mButton.setText(R.string.mantap);
+    }
+
+    public void closeFragment() {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        SimpleFragment simpleFragment = (SimpleFragment) fragmentManager.findFragmentById(R.id.fragment);
+
+        if (simpleFragment != null) {
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.fragment, simpleFragment).commit();
+        }
+
+        isFragmentDisplayed = false;
+        text = findViewById(R.id.content);
+        text.setText(R.string.content);
+        mButton.setText(R.string.b_kelebihan);
     }
 }
