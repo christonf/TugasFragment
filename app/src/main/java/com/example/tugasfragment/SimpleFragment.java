c58aab83eee8abd96f4e24bab0275b650623e70b
christonf <christophorus.ardhito0301@mail.ugm.ac.id>
2023-02-26 19:19:57 +0700

    Initial commit

---
+++ app/src/main/java/com/example/tugasfragment/SimpleFragment.java
@@ -0,0 +1,23 @@
+package com.example.tugasfragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SimpleFragment extends Fragment {
+    public SimpleFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        final View rootView = inflater.inflate(R.layout.fragment_simple, container, false);
+        return rootView;
+    }
+}
